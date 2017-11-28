ea6658cc9b1ad7b05a00ea64b53346b618060b6f
Henry Kenlay <kenlay@scotts-MacBook-Pro.local>
2017-11-28 13:04:11 +0000

    merged files

--- median_polish.cpp
+++ median_polish.cpp
@@ -2,3 +2,20 @@
 int() main{
 	
 }
+
+double median(Array<double,Dynamic,Dynamic> m, int index, bool isRow){
+	int size;
+	double mCopy[size];
+	isRow ? size = m.cols() : size = m.rows();
+	for(int i = 0; i < size; i++){
+		isRow ? mCopy[i] = m(index, i) : mCopy[i] = m(i, index); 
+	}
+
+	partial_sort(mCopy, &mCopy[1+size/2], &mCopy[size]);
+	if (size%2 == 0){
+		return 0.5*(mCopy[size/2] + mCopy[size/2 - 1]);
+	} else{
+		return mCopy[(size-1)/2];
+	}
+	
+}
