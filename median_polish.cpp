44af6100978e26fa00fef4b3417943112cea5a63
Henry Kenlay <kenlay@scotts-MacBook-Pro.local>
2017-11-29 12:03:29 +0000

    progress

--- median_polish.cpp
+++ median_polish.cpp
@@ -1,6 +1,54 @@
+#include <Eigen/Dense>
+#include <iostream>
+#include <fstream>
 
-int() main{
-	
+using namespace std;
+using namespace Eigen;
+
+void median_polish(Array<double,Dynamic,Dynamic>, double, int);
+ArrayXd median_of_each_row(Array<double,Dynamic,Dynamic> m);
+ArrayXd median_of_each_col(Array<double,Dynamic,Dynamic> m);
+double median(Array<double,Dynamic,Dynamic>, int, bool);
+ArrayXXd read_array(string);
+
+
+int main(){
+	ArrayXXd data = read_array("5_6");
+    median_polish(data, 0.01, 10);
+    return(0);
+}
+
+
+void median_polish(Array<double,Dynamic,Dynamic> z, double eps = 0.01, int maxiter = 10){
+    int nr = z.rows();
+    int nc = z.cols();
+    double t = 0.0;
+    ArrayXd r = ArrayXd::Zero(nr);
+    ArrayXd c = ArrayXd::Zero(nc);
+    double oldsum = 0.0;
+    for(int i = 0; i < maxiter; i++){
+        median_of_each_row(z);
+    }
+    return;
+}
+
+
+ArrayXd median_of_each_row(Array<double,Dynamic,Dynamic> m){
+    int nr = m.rows();
+    ArrayXd r = ArrayXd::Zero(nr);
+    for(int i = 0; i < nr; i++){
+        r(i) = median(m, i, true);
+    }
+    return r;
+}
+
+ArrayXd median_of_each_col(Array<double,Dynamic,Dynamic> m){
+    int nc = m.cols();
+    ArrayXd c = ArrayXd::Zero(nc);
+    for(int i = 0; i < nc; i++){
+        c(i) = median(m, i, false);
+    }
+    return c;
 }
 
 double median(Array<double,Dynamic,Dynamic> m, int index, bool isRow){
@@ -17,5 +65,27 @@ double median(Array<double,Dynamic,Dynamic> m, int index, bool isRow){
 	} else{
 		return mCopy[(size-1)/2];
 	}
-	
 }
+
+ArrayXXd read_array(string fname){
+	int n = stoi(fname.substr(0, fname.find("_")));
+	int m = stoi(fname.substr(fname.find("_")+1));
+	ArrayXXd data = ArrayXXd::Zero(n, m);
+	ifstream fs("data/" + fname);
+	string line;
+	int row = 0;
+	int column = 0;
+	while (fs >> line){
+		stringstream lineStream(line);
+		string cell;
+        while (getline(lineStream, cell, ',')){
+        	data(row, column) = stod(cell);
+        	column++;
+        }
+        column = 0;
+        row++;
+    }
+    fs.close();
+    return data;
+}
+
