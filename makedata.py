69b9e6565248bd7299dd179404fa70dde3af8638
Henry Kenlay <kenlay@scotts-MacBook-Pro.local>
2017-12-07 19:28:16 +0000

    modified test data

--- makedata.py
+++ makedata.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 def make_data(n, m):
     data = np.zeros((n, m))
-    row_means = np.random.random(n)*10;
-    col_means = np.random.random(m)*10;
+    row_means = np.random.random(n);
+    col_means = np.random.random(m);
+    global_effect = np.random.random();
     for i in range(n):
         for j in range(m):
-            data[i,j] = row_means[i] + col_means[j] + np.random.normal();
+            data[i,j] = global_effect + row_means[i] + col_means[j] + np.random.normal();
     pd.DataFrame(data).to_csv('data/{}_{}'.format(n, m), index = False, header = False)
     
-for i in range(1, 11):
-    make_data(i*100, i*100)
+if __name__ == '__main__':
+    for i in range(1, 11):
+        make_data(i*100, i*100)
