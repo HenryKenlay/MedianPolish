3f79b07566ae3ee752a0e2a6bf2940747bbe8977
Henry Kenlay <kenlay@user0389.eduroam.christs.private.cam.ac.uk>
2017-12-07 16:04:01 +0000

    removed unneeded line

--- median_polish.R
+++ median_polish.R
@@ -2,7 +2,6 @@ args <- commandArgs(TRUE)
 n <- toString(args[1])
 m <- toString(args[2])
 justread <- toString(args[3])
-fname = 
 data = read.table(paste("data/", n, "_", m, sep=""), sep = ',')
 if (justread == "NA"){
   invisible(medpolish(data,  trace.iter = FALSE))
