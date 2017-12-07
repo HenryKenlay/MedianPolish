f54637cfc106e2c4a0b94a6a3a8a83b47fece8ef
Henry Kenlay <kenlay@user0389.eduroam.christs.private.cam.ac.uk>
2017-12-07 16:06:29 +0000

    updated testrig

--- timeprograms.py
+++ timeprograms.py
@@ -1,13 +1,25 @@
 from subprocess import call
 from time import time
 import numpy as np
-import matplotlib.pyplot as plt 
+import pandas as pd
 
-def time_program(n,m, prog = "R", repeats = 100, return_means = True):
-    if prog != "R":
-        program = ["./{}".format(prog),]
+def benchmark(prog, repeats, save = False):
+    dfs = []
+    for i in np.arange(100, 1100, 100):
+        a, b = time_program(i, i, prog, repeats, False)
+        df = pd.DataFrame({'total' : a, 'read-time' : b}, index = [i for _ in range(repeats)])
+        dfs.append(df)
+    df = pd.concat(dfs)
+    df.index.name = 'input-size'
+    if save:
+        df.to_csv('results/{}.csv'.format(prog))
+    return df
+
+def time_program(n,m, prog = 'R', repeats = 100, return_means = True):
+    if prog != 'R':
+        program = ['./{}.out'.format(prog),]
     else:
-        program = ["/usr/local/bin/Rscript", "median_polish.R"]
+        program = ['/usr/local/bin/Rscript', 'median_polish.R']
     prog_timings = []
     read_timings = []
     for _ in range(repeats):
@@ -15,15 +27,15 @@ def time_program(n,m, prog = "R", repeats = 100, return_means = True):
         call(program + [str(n), str(m)])
         prog_timings.append(time() - start)
         start = time()
-        call(program + [str(n), str(m), "0"])
+        call(program + [str(n), str(m), '0'])
         read_timings.append(time() - start)
     if return_means:
         return np.mean(prog_timings), np.mean(read_timings)
     else:
         return prog_timings, read_timings
 
-#print(time_program(1000,1000, repeats = 5))
-for i in np.arange(100, 1100, 100):    
-    _, v2_time = time_program(i,i, './median_polish-v2.out', 20, False)
-    _, v3_time = time_program(i,i, './median_polish-v3.out', 20, False)
-    print(mannwhitneyu(np.asarray(v2_time), np.asarray(v3_time), alternative = 'greater'))
+if __name__ == '__main__':
+    benchmark('R', 100, True)
+    benchmark('median_polish-v1', 100, True)
+    benchmark('median_polish-v2', 100, True)
+    benchmark('median_polish-v3', 100, True)
